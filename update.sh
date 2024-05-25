b243214eb87204106f9a01a8ec469d6928ad5fb8
Rafael <github@rafaelcecilio.com>
2024-05-25 09:18:12 -0400

    Fix user permissions.

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo -S purge &&
 sudo -S softwareupdate -i -a -R --agree-to-license --verbose &&
-sudo -u rafael brew upgrade &&
-sudo -u rafael brew update &&
-sudo -u rafael brew list | xargs brew install &&
-sudo -u rafael brew cleanup --prune=all &&
-sudo -u rafael brew autoremove &&
+sudo -u $USER brew upgrade &&
+sudo -u $USER brew update &&
+sudo -u $USER brew list | xargs brew install &&
+sudo -u $USER brew cleanup --prune=all &&
+sudo -u $USER brew autoremove &&
 sudo /usr/sbin/periodic daily weekly monthly &&
 sudo -S purge
