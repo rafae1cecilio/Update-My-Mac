0a54f4a1009da213939844cc0c0357d90321fe5b
Rafael Cecilio <62517727+rafae1cecilio@users.noreply.github.com>
2024-12-01 21:56:22 -0500

    Update update.sh
    comestic fix

--- update.sh
+++ update.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 sudo -S purge &&
 sudo -S softwareupdate -i -a -R --agree-to-license --verbose &&
+sudo -u $USER brew update &&
 sudo -u $USER brew upgrade &&
 sudo -u $USER brew update &&
 sudo -u $USER brew list | xargs brew install &&
 sudo -u $USER brew cleanup --prune=all &&
+sudo -u $USER brew tap --repair &&
 sudo -u $USER brew autoremove &&
 sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder &&
 sudo -S purge
