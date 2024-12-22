f5330359ef3bbe2f9291ca85ba7d535dd9e11900
Rafael Cecilio <62517727+rafae1cecilio@users.noreply.github.com>
2024-12-22 10:42:15 -0500

    Update update.sh
    Add remove known_hosts

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo -S purge &&
-sudo -S softwareupdate -i -a -R --agree-to-license --verbose &&
+rm /Users/$USER/.ssh/known_hosts &&
 sudo -u $USER brew update &&
 sudo -u $USER brew upgrade &&
 sudo -u $USER brew update &&
@@ -8,5 +8,7 @@ sudo -u $USER brew list | xargs brew install &&
 sudo -u $USER brew cleanup --prune=all &&
 sudo -u $USER brew tap --repair &&
 sudo -u $USER brew autoremove &&
-sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder &&
+sudo -S dscacheutil -flushcache &&
+sudo -S killall -HUP mDNSResponder &&
+sudo -S softwareupdate -i -a -R --agree-to-license --verbose &&
 sudo -S purge
